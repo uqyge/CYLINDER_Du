077261253f7400964566162c59fdb0b4c252c171
eg <cqu>
2021-12-23 23:35:52 +0800

    postprocessing on one case

--- python/post.py
+++ python/post.py
@@ -1,50 +1,64 @@
 #%%
-import scipy.io as sio
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
+import pandas as pd
+import plotly.express as px
+import plotly.io as pio
+import scipy.io as sio
+
+pio.renderers.default = "notebook_connected"
 
 #%%
-geo = pd.read_csv("../../case_0/geo.csv", names=["t", "R"], header=None)
-geo
+case = 0
+geo = pd.read_csv(f"../../case_{case}/geo.csv", names=["t", "R"], header=None)
+# geo
+# %%
+doe_res = sio.loadmat(f"../../case_{case}/doe_res.mat")
+# doe_res
 
 # %%
-doe_res = sio.loadmat("../../case_0/doe_res.mat")
+df_case = pd.DataFrame()
+for i in range(len(geo)):
+    # i = 0
+    # print(i, geo.iloc[i])
+
+    geo_val = geo.values[i]
+    u = -doe_res["doe_res"]["u"][0, i][1::].reshape(-1)
+    load = -doe_res["doe_res"]["load"][0, i][1::].reshape(-1)
+    incr = np.asarray(range(len(u)))
+    incr_u_load = np.vstack([incr, u, load]).T
+
+    FAILDISP = (doe_res["doe_res"]["res"][0, i]["FAILDISP"],)
+    FAILLOAD = (doe_res["doe_res"]["res"][0, i]["FAILLOAD"],)
+
+    # print(FAILDISP, FAILLOAD)
+    # print(u[-1], load[-1])
+
+    assert (FAILLOAD == load[-1]) & (FAILDISP == u[-1])
+    # plt.plot(u, load)
+    # plt.show()
+
+    data = np.hstack([incr_u_load, np.ones(len(u)).reshape(-1, 1) * geo_val])
+    df_incr = pd.DataFrame(data, columns=["incr", "u", "load"] + geo.columns.tolist())
+    df_case = pd.concat([df_case, df_incr])
+# plt.plot(df_incr.u, df_incr.load)
+df_case.reset_index(inplace=True)
+# df_case.tail()
+# %%
+for R in df_case["R"].unique():
+    df_curve = df_case[df_case["R"] == R]
+    # print(R,df_case[df_case['R']==R].u.shape)
+    plt.plot(df_curve.u, df_curve.load)
 
 # %%
-doe_res
+case_incr_max = [
+    df_case["incr"][df_case["R"] == unique_R].idxmax()
+    for unique_R in df_case["R"].unique()
+]
 
 # %%
-# plt.plot(-doe_res["doe_res"]["u"][0, 0], -doe_res["doe_res"]["load"][0, 0])
-i=39
-print(i,geo.iloc[i])
-u = -doe_res["doe_res"]["u"][0, i]
-load = -doe_res["doe_res"]["load"][0, i]
-plt.plot(u, load)
-# %%
-doe_res["doe_res"]["u"][0, :].shape
-# %%
+px.scatter_3d(df_case.iloc[case_incr_max], x="R", y="t", z="load")
 
-# %%
-geo.iloc[39]
-# %%
-u = doe_res["doe_res"]["u"][0,:]
-load = doe_res["doe_res"]["load"][0,:]
-# %%
-u[0].reshape(-1)
-load[0].reshape(-1)
-# %%
-load[0]
-# %%
-a = np.vstack([-u[0].reshape(-1),-load[0].reshape(-1)]).T
-a.shape
-# %%
-
-# %%
-data = np.hstack([a,np.ones([a.shape[0],1])*geo.values[0]])
-data.shape
-# %%
-plt.plot(-data[:,0],-data[:,1])
-# %%
-
+#%%
+df_case[['R','t']].unique()
 # %%
